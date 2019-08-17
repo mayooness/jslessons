fce8325318c338cdbe5ee5b629552b036037ba9f
unknown <mayooness@gmail.com>
2019-08-17 14:39:31 +0300

    first lesson

---
+++ script.js
@@ -0,0 +1,17 @@
+let Tc = prompt("Температура по Цельсию");
+Tf = (9/5)*Tc + 32;
+alert("Температура по Фарингейту равна = " + Tf);
+// при озвучивание д/з вы просили использовать parseInt поясните пожалуйста зачем ведь и так все работает
+
+let name = "Василий";
+let admin = name;
+console.log(admin);
+
+let a = 10;
+let b = 20;
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a);
+console.log(b);
